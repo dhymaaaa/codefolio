db5e6c4950ce2f52a7d9abba41ae9ec2165ebbab
dhymaaaa <dheemahussain44@gmail.com>
2025-01-04 15:28:32 +0500

    change color

--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -7,7 +7,7 @@ const Main = () => {
     return (
         <main id='content' className='pt-24 lg:w-[52%] lg:py-24'>
             <section id='about' className='mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24' aria-label='About me'>
-                <div className='sticky top-0 z-20 -mx-6 mb-4 w-screen bg-[#060c1c] px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0'>
+                <div className='sticky top-0 z-20 -mx-6 mb-4 w-screen bg-[#1b1947] px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0'>
                     <h2 className='text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only'>About</h2>
                 </div>
                 <div>
@@ -16,18 +16,18 @@ const Main = () => {
                     </p>
                     <p className='mb-4'>
                         Currently, I&apos;m an intern at
-                        <a className='font-medium text-slate-200 hover:text-teal-300 focus-visible:text-teal-300 ml-1 mr-1' href="https://corporate.visitmaldives.com/" target='_blank' rel='noreferrer noopener'>
-                            Maldives Marketing & PR Corporation (MMPRC),
+                        <a className='font-medium text-slate-200 hover:text-[#9290C3] focus-visible:text-[#9290C3] ml-1 mr-1' href="https://corporate.visitmaldives.com/" target='_blank' rel='noreferrer noopener'>
+                            Maldives Marketing & PR Corporation,
                         </a>
                         contributing in the UI design of one of their websites.
                     </p>
                     <p>
-                        I&apos;ve worked on projects like HRMS systems and dynamic web apps using technologies like Vue, Laravel, React, and Python. I&apos;m currently exploring AI and it&apos;s applications in language learning for Dhihvehi language learners.
+                        I&apos;ve worked on projects like HRMS systems and dynamic web apps using technologies like Vue, Laravel, React, and Python. I&apos;m currently exploring AI and it&apos;s applications in language learning for Dhivehi language learners.
                     </p>
                 </div>
             </section>
             <section id='experience' className='mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24' aria-label='Work experience'>
-                <div className='sticky top-0 z-20 -mx-6 mb-4 w-screen bg-[#060c1c] px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0'>
+                <div className='sticky top-0 z-20 -mx-6 mb-4 w-screen bg-[#1b1947] px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0'>
                     <h2 className='text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only'>Experience</h2>
                 </div>
                 <ol className='group/list'>
@@ -37,9 +37,9 @@ const Main = () => {
                             <header className='z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2' aria-label='2024 to present'> 27 October 2024 to Present</header>
                             <div className='z-10 sm:col-span-6'>
                                 <h3 className='font-medium leading-snug text-slate-200'>
-                                    <a className='inline-flex items-center font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base' href="https://corporate.visitmaldives.com/" target='_blank' rel='noreferrer noopener' aria-label='Intern, MMPRC'>
+                                    <a className='inline-flex items-center font-medium leading-tight text-slate-200 hover:text-[#9290C3] focus-visible:text-[#9290C3]  group/link text-base' href="https://corporate.visitmaldives.com/" target='_blank' rel='noreferrer noopener' aria-label='Intern, MMPRC'>
                                         Intern -
-                                        <span className="ml-1">MMPRC</span>
+                                        <span className="ml-1">Maldives Marketing & PR Corporation</span>
                                         <Icon path={mdiArrowTopRight} size={0.75} className='ml-1' />
                                     </a>
                                 </h3>
@@ -48,27 +48,27 @@ const Main = () => {
                                 </p>
                                 <ul className='mt-2 flex flex-wrap' aria-label='Technologies used'>
                                     <li className='mr-1.5 mt-2'>
-                                        <div className='flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 '>
+                                        <div className='flex items-center rounded-full bg-[#535C91] px-3 py-1 text-xs font-medium leading-5 text-[#dbc5f2] '>
                                             Next.js
                                         </div>
                                     </li>
                                     <li className='mr-1.5 mt-2'>
-                                        <div className='flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 '>
+                                        <div className='flex items-center rounded-full bg-[#535C91] px-3 py-1 text-xs font-medium leading-5 text-[#dbc5f2] '>
                                             Nuxt.js
                                         </div>
                                     </li>
                                     <li className='mr-1.5 mt-2'>
-                                        <div className='flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 '>
+                                        <div className='flex items-center rounded-full bg-[#535C91] px-3 py-1 text-xs font-medium leading-5 text-[#dbc5f2] '>
                                             Figma
                                         </div>
                                     </li>
                                     <li className='mr-1.5 mt-2'>
-                                        <div className='flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 '>
+                                        <div className='flex items-center rounded-full bg-[#535C91] px-3 py-1 text-xs font-medium leading-5 text-[#dbc5f2] '>
                                             Firebase
                                         </div>
                                     </li>
                                     <li className='mr-1.5 mt-2'>
-                                        <div className='flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 '>
+                                        <div className='flex items-center rounded-full bg-[#535C91] px-3 py-1 text-xs font-medium leading-5 text-[#dbc5f2] '>
                                             HTML & CSS
                                         </div>
                                     </li>
@@ -82,7 +82,7 @@ const Main = () => {
                             <header className='z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2' aria-label='2024 to present'> 26 October 2023 to 25 April 2024</header>
                             <div className='z-10 sm:col-span-6'>
                                 <h3 className='font-medium leading-snug text-slate-200'>
-                                    <a className='inline-flex items-center font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base' href="https://pgoffice.gov.mv/" target='_blank' rel='noreferrer noopener' aria-label='Intern, Prosecutor Generals Office'>
+                                    <a className='inline-flex items-center font-medium leading-tight text-slate-200 hover:text-[#9290C3] focus-visible:text-[#9290C3]  group/link text-base' href="https://pgoffice.gov.mv/" target='_blank' rel='noreferrer noopener' aria-label='Intern, Prosecutor Generals Office'>
                                         Intern -
                                         <span className="ml-1">Prosecutor General&apos;s Office</span>
                                         <Icon path={mdiArrowTopRight} size={0.75} className='ml-1' />
@@ -93,17 +93,17 @@ const Main = () => {
                                 </p>
                                 <ul className='mt-2 flex flex-wrap' aria-label='Technologies used'>
                                     <li className='mr-1.5 mt-2'>
-                                        <div className='flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 '>
+                                        <div className='flex items-center rounded-full bg-[#535C91] px-3 py-1 text-xs font-medium leading-5 text-[#dbc5f2] '>
                                             Laravel
                                         </div>
                                     </li>
                                     <li className='mr-1.5 mt-2'>
-                                        <div className='flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 '>
+                                        <div className='flex items-center rounded-full bg-[#535C91] px-3 py-1 text-xs font-medium leading-5 text-[#dbc5f2] '>
                                             Vue.js
                                         </div>
                                     </li>
                                     <li className='mr-1.5 mt-2'>
-                                        <div className='flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 '>
+                                        <div className='flex items-center rounded-full bg-[#535C91] px-3 py-1 text-xs font-medium leading-5 text-[#dbc5f2] '>
                                             MySQL
                                         </div>
                                     </li>
@@ -112,7 +112,7 @@ const Main = () => {
                         </div>
                     </li>
                     <li className='mb-12'>
-                        <a className='inline-flex items-center font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300 group/link text-base' href="/files/Resume.pdf">
+                        <a className='inline-flex items-center font-medium leading-tight text-slate-200 hover:text-[#9290C3] focus-visible:text-[#9290C3] group/link text-base' href="/files/Resume.pdf" target='_blank'>
                             View Full -
                             <span className="ml-1">CV</span>
                             <Icon path={mdiArrowTopRight} size={0.75} className='ml-1' />
@@ -121,7 +121,7 @@ const Main = () => {
                 </ol>
             </section>
             <section id='projects' className='mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24' aria-label='Selected projects'>
-                <div className='sticky top-0 z-20 -mx-6 mb-4 w-screen bg-[#060c1c] px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0'>
+                <div className='sticky top-0 z-20 -mx-6 mb-4 w-screen bg-[#1b1947] px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0'>
                     <h2 className='text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only'>Projects</h2>
                 </div>
                 <ul className='group/list'>
@@ -130,7 +130,7 @@ const Main = () => {
                             <div className='absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg'></div>
                             <div className='z-10 sm:order-2 sm:col-span-6'>
                                 <h3>
-                                    <a className='inline-flex items-center font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base' href="https://github.com/dhymaaaa/Discover-Maldives" target='_blank' rel='noreferrer noopener' aria-label='Discover Maldives website'>
+                                    <a className='inline-flex items-center font-medium leading-tight text-slate-200 hover:text-[#9290C3] focus-visible:text-[#9290C3]  group/link text-base' href="https://github.com/dhymaaaa/Discover-Maldives" target='_blank' rel='noreferrer noopener' aria-label='Discover Maldives website'>
                                         <span className="ml-1">Discover Maldives website (GitHub repository)</span>
                                         <Icon path={mdiArrowTopRight} size={0.75} className='ml-1' />
                                     </a>
@@ -140,17 +140,17 @@ const Main = () => {
                                 </p>
                                 <ul className='mt-2 flex flex-wrap' aria-label='Languages used'>
                                     <li className='mr-1.5 mt-2'>
-                                        <div className='flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 '>
+                                        <div className='flex items-center rounded-full bg-[#535C91] px-3 py-1 text-xs font-medium leading-5 text-[#dbc5f2] '>
                                             PHP
                                         </div>
                                     </li>
                                     <li className='mr-1.5 mt-2'>
-                                        <div className='flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 '>
+                                        <div className='flex items-center rounded-full bg-[#535C91] px-3 py-1 text-xs font-medium leading-5 text-[#dbc5f2] '>
                                             JavaScript
                                         </div>
                                     </li>
                                     <li className='mr-1.5 mt-2'>
-                                        <div className='flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 '>
+                                        <div className='flex items-center rounded-full bg-[#535C91] px-3 py-1 text-xs font-medium leading-5 text-[#dbc5f2] '>
                                             HTML & CSS
                                         </div>
                                     </li>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,13 @@ import Main from "./components/Main";
 
 export default function Home() {
   return (
-    <div className="lg:flex lg:justify-between lg:gap-4">
-      <Header />
-      <Main />
+    <div className="__variable_20b187 group/spotlight relative">
+      <div className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-16 lg:py-0">
+        <div className="lg:flex lg:justify-between lg:gap-4">
+          <Header />
+          <Main />
+        </div>
+      </div>
     </div>
   );
 }
