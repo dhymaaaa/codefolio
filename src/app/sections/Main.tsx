2f4158beb2cced051ad80afbbf0a37c2e3e48801
dhymaaaa <dheemahussain44@gmail.com>
2025-02-10 12:16:09 +0500

    intership duration update

--- src/app/sections/Main.tsx
+++ src/app/sections/Main.tsx
@@ -34,7 +34,7 @@ const Main = () => {
                     <li className='mb-12'>
                         <div className='group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50'>
                             <div className='absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg'></div>
-                            <header className='z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2' aria-label='2024 to present'> 27 October 2024 to Present</header>
+                            <header className='z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2' aria-label='2024 to present'> 27 October 2024 to 26 January 2025</header>
                             <div className='z-10 sm:col-span-6'>
                                 <h3 className='font-medium leading-snug text-slate-200'>
                                     <a className='inline-flex items-center font-medium leading-tight text-[#e9c073] hover:text-[#FFA500] focus-visible:text-[#FFA500]  group/link text-base' href="https://corporate.visitmaldives.com/" target='_blank' rel='noreferrer noopener' aria-label='Intern, MMPRC'>
